c117a927a9b29e1fb2d07c066269b955ac7ef51e
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-10-31 22:32:45 -0700

    Create NATO-alphabet.js

---
+++ NATO-alphabet.js
@@ -0,0 +1,19 @@
+/*
+Task:
+  You'll have to translate a string to Pilot's alphabet (NATO phonetic alphabet).
+
+Input:
+  If, you can read?
+
+Output:
+  India Foxtrot , Yankee Oscar Uniform Charlie Alfa November Romeo Echo Alfa Delta ?
+
+Note:
+  There is a preloaded dictionary that you can use, named NATO. 
+  It uses uppercase keys, e.g. NATO['A'] is "Alfa".
+  The set of used punctuation is ,.!?.
+  Punctuation should be kept in your return string, but spaces should not.
+  Xray should not have a dash within.
+  Every word and punctuation mark should be seperated by a space ' '.
+  There should be no trailing whitespace.
+*/
